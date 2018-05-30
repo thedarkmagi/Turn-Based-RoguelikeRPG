a25e4e6c42629f7bd540d17f9bb4c6a56e4958c1
DigitalDok <kdokos@hotmail.com>
2018-05-30 22:30:05 +0100

    Inventory Test
    Inventory Starting Script
    Scriptable Objects as Inventory Items

---
+++ Turn-Based-RoguelikeRPG/Assets/Scripts/Inventory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+
+    public Item TestItem;
+
+    public List<Item> Backpack = new List<Item>(0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) AddItem(TestItem);
+        if (Input.GetKeyDown(KeyCode.X)) RemoveItem(TestItem);
+	}
+
+    #region Inventory Management
+
+    public void AddItem(Item ItemToAdd)
+    {
+        if (Backpack.Count < 20)
+            Backpack.Add(ItemToAdd);
+        else
+            print("Not enough space.");
+    }
+
+    public void RemoveItem(Item ItemToRemove)
+    {
+        int IndexToRemove = GetIndexOfItem(ItemToRemove);
+        if (IndexToRemove != -1)
+        {
+            Backpack.RemoveAt(IndexToRemove);
+        }
+    }
+
+    public int GetIndexOfItem(Item ItemToCheck)
+    {
+        for (int i = 0; i < Backpack.Count; i++)
+        {
+            if (Backpack[i] == ItemToCheck) return i;
+        }
+
+        return -1;
+    }
+
+    public int GetIndexOfItem(string ItemToCheck)
+    {
+        for (int i = 0; i < Backpack.Count; i++)
+        {
+            if (Backpack[i].ItemName == ItemToCheck) return i;
+        }
+
+        return -1;
+    }
+
+    #endregion
+
+}
+
+
---
+++ Turn-Based-RoguelikeRPG/Assets/Scripts/Item.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Item : ScriptableObject
+{
+    public string ItemName;
+    public string ItemDesc;
+
+    public enum ItemType
+    {
+        Consumable,
+        Gear,
+        KeyItem
+    }
+	
+}
+
+[CreateAssetMenu]
+public class Consumable : Item
+{
+    public float HP_Gain;
+    public float MP_Gain;
+}
+
+[CreateAssetMenu]
+public class Gear : Item
+{
+    public enum GearType
+    {
+        Head,
+        Shoulders,
+        Gloves,
+        Waist,
+        Chest,
+        Legs,
+        Feet,
+        Back,
+        RingA,
+        RingB,
+        HandA,
+        HandB
+    }
+
+    public GearType MyType;
+
+    public float Fortify_STR;
+    public float Fortify_DEX;
+    public float Fortify_INT;
+    public float Fortify_WIS;
+    public float Fortify_CON;
+    public float Fortify_CHA;
+
+    public float Fortify_DMG;
+    public float Fortify_SPEED;
+    public float Fortify_AC;
+
+}
+
+[CreateAssetMenu]
+public class KeyItem : Item
+{
+
+}
